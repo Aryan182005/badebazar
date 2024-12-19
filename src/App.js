197f3d3aa4a50d1941d5df45ff4e2745ab28e9a3
unknown <aryanchodvadiya0990@gmail.com>
2024-12-19 18:22:43 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './pages/home';
+import Aboutsectoion from './components/aboutsectoion';
+
+function App() {
+  return (
+    <div className="">
+      <Home />
+
+ </div>
+  );
+}
+
+export default App;
+
---
+++ src/pages/home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Banner from '../components/banner'
+import Header from '../components/header'
+import Whychoose from '../components/whychoose'
+import Aboutsectoion from '../components/aboutsectoion'
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Banner />
+      <Aboutsectoion />
+      <Whychoose />
+    </div>
+  )
+}
+
+export default Home
